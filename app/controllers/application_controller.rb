17acfdcc0f67631b52cdac8db8ad50494754d717
reyes2981 <noe.reyes92@yahoo.com>
2021-02-14 21:29:36 -0600

    commit tree c07266df19ab47408bc7be4522ff407ea56cca99

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,24 @@
 class ApplicationController < Sinatra::Base
+
+  set :views, Proc.new { File.join(root, "../views/") } #need to find out what this does
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes/new' do #loads new form
+    erb :new
+  end
+
+  get '/recipes' do #loads index page
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id' do  #loads show page
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
 
 end
